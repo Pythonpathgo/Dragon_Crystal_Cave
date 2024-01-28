eeccbe06225a4d06cf1c844861108b7e1e7f5755
codingpath <pythongpath@gmail.com>
2024-01-27 17:06:37 -0800

    update

---
+++ dragon.py
@@ -0,0 +1,68 @@
+print ('''
+***************************************************************************************************       
+                        /\
+                        ||
+                        ||
+                        ||
+                        ||                                               ~-----~
+                        ||                                            /===--  ---~~~
+                        ||                   ;'                 /==~- --   -    ---~~~
+                        ||                (/ ('              /=----         ~~_  --(  '
+                        ||             ' / ;'             /=----               \__~
+     '                ~==_=~          '('             ~-~~      ~~~~        ~~~--\~'
+     \\                (c_\_        .i.             /~--    ~~~--   -~     (     '
+      `\               (}| /       / : \           / ~~------~     ~~\   (
+      \ '               ||/ \      |===|          /~/             ~~~ \ \(
+      ``~\              ~~\  )~.~_ >._.< _~-~     |`_          ~~-~     )
+       '-~                 {  /  ) \___/ (   \   |` ` _       ~~         '
+       \ -~\                -<__/  -   -  L~ -;   \\    \ _ _/
+       `` ~~=\                  {    :    }\ ,\    ||   _ :(
+        \  ~~=\__                \ _/ \_ /  )  } _//   ( `|'
+        ``    , ~\--~=\           \     /  / _/ / '    (   '
+         \`    } ~ ~~ -~=\   _~_  / \ / \ )^ ( // :_  / '
+         |    ,          _~-'   '~~__-_  / - |/     \ (
+          \  ,_--_     _/              \_'---', -~ .   \
+           )/      /\ / /\   ,~,         \__ _}     \_  "~_
+           ,      { ( _ )'} ~ - \_    ~\  (-:-)       "\   ~ 
+                  /'' ''  )~ \~_ ~\   )->  \ :|    _,       " 
+                 (\  _/)''} | \~_ ~  /~(   | :)   /          }
+                <``  >;,,/  )= \~__ {{{ '  \ =(  ,   ,       ;
+               {o_o }_/     |v  '~__  _    )-v|  "  :       ,"
+               {/"\_)       {_/'  \~__ ~\_ \\_} '  {        /~
+               ,/!          '_/    '~__ _-~ \_' :  '      ,"  ~ 
+              (''`                  /,'~___~    | /     ,"  \ ~' 
+             '/, )                 (-)  '~____~";     ,"     , }
+           /,')                    / \         /  ,~-"       '~'
+       (  ''/                     / ( '       /  /          '~'
+    ~ ~  ,, /) ,                 (/( \)      ( -)          /~'
+  (  ~~ )`  ~}                   '  \)'     _/ /           ~'
+ { |) /`,--.(  }'                    '     (  /          /~'
+(` ~ ( c|~~| `}   )                        '/:\         ,'
+ ~ )/``) )) '|),                          (/ | \)                 -sjm
+  (` (-~(( `~`'  )                        ' (/ '
+   `~'    )'`')                              '
+     ` ``
+**************************************************************************************************
+''')
+print("Welcome to once and life time adventure to Dragon Crystal Cave.")
+print("Your mission is to find the treasure.")
+
+question1 = input('You\'re inside the cave, where do you want to go? Type "left" or "right" .\n').lower()
+
+if question1 =="left":
+ question2 = input('You\'ve come to two dungens doors. There is red door and blue door. Which one will you choose? Type "red" or "blue".\n').lower() 
+ if question2 == "blue":
+  question3 = input('You have found the sword of Destiny which also likes to talk your ear off. Because its alive and lonely. Choose to fight the dragon or flee. Type "fight" or "flee".\n').lower()
+  if question3 == "fight":
+   print("You fight the dragon and barley escaped with your life but did not find the treasure. Thank you for playing.\n") 
+  elif question3 == "flee":
+   print("You ranaway and found the treasure room! Thank you for playing.\n")
+   
+ else:
+  print("You encounter a hungry horde of goblins and die. Game over.")
+else:
+ print("You fell into a pit full of snakes. Game over.")
+ 
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+ 
+ 
